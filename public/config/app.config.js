615fba0126d1ce7993a4dc3179043e5c25141215
Nicholas Barger <nicholas@nicholasbarger.com>
2013-11-09 21:53:54 -0500

    Added some angular structure and split the item page into partials because it is so large.

---
+++ public/config/app.config.js
@@ -0,0 +1,1 @@
+var bluewater = angular.module('bluewater', []);
--- public/controllers/item.controller.js
+++ public/controllers/item.controller.js
@@ -1,10 +1,19 @@
-function ItemController($scope) {
-    
-    $scope.showRemoveIcon = function(targetId) {
-        $(targetId).show();
-    };
-    
-    $scope.hideRemoveIcon = function(targetId) {
-        $(targetId).hide();
-    };
-}
+bluewater.controller('ItemController', function ($scope) {
+
+    // close a tab (because we allow multiple tabs opening when selecting items)
+    $scope.closeTab = function(tabId, paneId) {
+
+        var tab = document.getElementById(tabId);
+        var pane = document.getElementById(paneId);
+        var remainingTab = tab.previousElementSibling;
+
+        // remove the tab
+        tab.parentNode.removeChild(tab);
+
+        // remove the tab pane
+        pane.parentNode.removeChild(pane);
+
+        // switch active tab to one to the left (if available)
+        $(remainingTab).children('a').tab('show');
+    }
+});
